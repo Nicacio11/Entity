af8ae048a76b8683d4d26a433654c5a772314961
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-05-30 12:05:09 -0300

    feat: :sparkles: add entity log on console

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -17,7 +17,10 @@ namespace Blog.Data
         public DbSet<User> Users { get; set; }
         // public DbSet<UserRole> UserRoles { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
             optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True");
+            optionsBuilder.LogTo(Console.WriteLine);
+        }
     }
 }
