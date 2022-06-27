a4057ec9a8de8f38a079e7a3cef6ea3356b7a5f1
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-06-27 17:08:26 -0300

    feat: :sparkles: add pagination

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Blog.Data;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Blog
 {
@@ -51,15 +52,34 @@ namespace Blog
             // context.Posts.Add(post);
             // context.SaveChanges();
 
-            var posts = context.Posts
-            .Include(x => x.Author)
-            .Include(x => x.Category)
-            .OrderBy(x => x.LastUpdateDate)
-            .ToList();
+            // var posts = context.Posts
+            // .Include(x => x.Author)
+            // .Include(x => x.Category)
+            // .OrderBy(x => x.LastUpdateDate)
+            // .ToList();
 
+            // var posts = context.Posts.AsNoTracking()
+            // .Include(x => x.Author)
+            // .Include(x => x.Category)
+            // .OrderBy(x => x.LastUpdateDate)
+            // .ToList();
+
+            var posts = GetPosts(context);
 
             foreach(var post in posts)
                 Console.WriteLine($"{post.Title} escrito por {post.Author.Name}");
+        }
+
+
+        public static List<Post> GetPosts(BlogDataContext context, int skip = 0, int take = 1000)
+        {
+            var posts = context.Posts
+            .AsNoTracking()
+            .Skip(skip)
+            .Take(take)
+            .ToList();
+
+            return posts;
         }
     }
 }
