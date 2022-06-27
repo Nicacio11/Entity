a50f72106cb17b5833083ca77114f2c3209e6207
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-06-27 11:48:15 -0300

    feat: :sparkes: add migrations

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -20,7 +20,7 @@ namespace Blog.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=BlogMigration;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True");
             optionsBuilder.LogTo(Console.WriteLine);
         }
 
